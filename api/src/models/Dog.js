9f7d416df912c81846209021121ed5c2f1fa2f3c
Francisco.Deville <frandeville4@gmail.com>
2022-06-19 20:11:36 -0300

    commit

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,48 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height_min: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    height_max: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight_min: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight_max: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    life_span_min: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    life_span_max: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    }
   });
 };
